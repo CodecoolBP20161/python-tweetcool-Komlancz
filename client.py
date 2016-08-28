73277aa33748a5c2287936c18634b447a9f690a3
Komlancz Janos <jkomlancz@gmail.com>
2016-08-28 16:15:34 +0200

    I think it works

--- client.py
+++ client.py
@@ -1,5 +1,10 @@
 import argparse
 import ipaddress
+import requests
+import json
+import datetime
+import subprocess
+
 
 
 parser = argparse.ArgumentParser()
@@ -28,3 +33,49 @@ if not(1024 < server["port"] < 65535):
 server["address"] = 'http://' + server["host"].compressed + ':' + str(server["port"])
 
 # Logic starts here... somewhere..
+
+my_route = server["address"]+"/tweet"
+user = 'code'
+
+
+def get_tweets():
+    get_it = requests.get(my_route)
+    to_json = get_it.json()
+    return to_json
+
+
+def tweet_formatter():
+    json_data = get_tweets()
+    for i in json_data:
+        print("")
+        print(i['poster'], "<" + str(datetime.datetime.utcfromtimestamp(int(i['timestamp']))) + ">:", i['content'])
+    print("")
+
+
+def post_tweet(message):
+    message = str(message)
+    payload = {"content": message, "poster": user}
+    requests.post(my_route, json=payload)
+
+
+def menu():
+    print('Available commands : \n refresh : Refresh the lates tweets.\n exit : Exit the program.\n post : Post a tweet.')
+    command = input('command: ')
+    return command
+
+tweet_formatter()
+while True:
+    try:
+        command = menu()
+        if command == 'refresh':
+            tweet_formatter()
+        elif command == 'exit':
+            exit()
+        elif command == 'post':
+            message = input('Type your message: ')
+            post_tweet(message)
+            tweet_formatter()
+    except EOFError:
+        exit()
+    except KeyboardInterrupt:
+        exit()
